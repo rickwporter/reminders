e48d31565982ecee74b9c64c09ffe8e557c1dac5
Rick Porter <rickwporter@gmail.com>
2024-03-31 12:22:11 -0400

    Find user unit test

--- test_reminders.py
+++ test_reminders.py
@@ -2,6 +2,7 @@ import unittest
 
 from datetime import datetime
 from reminders import Reminders
+from reminders import ROW_HEADER
 
 
 class TestReminders(unittest.TestCase):
@@ -30,9 +31,45 @@ class TestReminders(unittest.TestCase):
         uut = Reminders()
         items = uut.sheet_to_dict('example/bedrock.xlsx', 'Users')
         self.assertEqual(4, len(items))
+        keys = set(['User', 'First', 'Email', 'Aliases', ROW_HEADER])
         first = items[0]
+        self.assertEqual(keys, set(first.keys()))
         self.assertEqual('Fred Flintstone', first.get('User'))
         self.assertEqual('Fred', first.get('First'))
         self.assertEqual('fred@slaterockandgravel.com', first.get('Email'))
         self.assertEqual('FF', first.get('Aliases'))
         self.assertEqual('Users:1', first.get('_row'))
+
+    def test_reminders_find_user(self):
+        uut = Reminders()
+        users = uut.sheet_to_dict('example/bedrock.xlsx', 'Users')
+        self.assertEqual(4, len(users))
+        fred = users[0]  # Fred is the first row
+        wilma = users[1]
+        barney = users[2]
+        betty = users[3]
+
+        # find Fred
+        self.assertEqual(fred, uut.findUser(users, 'Fred'))
+        self.assertEqual(fred, uut.findUser(users, 'FF'))  # aliases
+        self.assertEqual(fred, uut.findUser(users, 'ff'))  # aliases, lowercase
+        self.assertEqual(fred, uut.findUser(users, 'FreD@slate'))  # email case insensitive
+        self.assertEqual(fred, uut.findUser(users, 'Fred flint'))  # user case insensitive
+
+        # TODO: fix duplicate error -- should be an error
+        self.assertEqual(fred, uut.findUser(users, 'slate'))
+        self.assertEqual(fred, uut.findUser(users, 'flintstone'))
+
+        # find Wilma
+        self.assertEqual(wilma, uut.findUser(users, 'Wilma F'))
+        self.assertEqual(wilma, uut.findUser(users, 'wf'))  # aliases
+        self.assertEqual(wilma, uut.findUser(users, 'wilma.Fl'))  # email case insensitive
+
+        # find Barney
+        self.assertEqual(barney, uut.findUser(users, 'Barney R'))
+        self.assertEqual(barney, uut.findUser(users, 'BR'))  # aliases
+        self.assertEqual(barney, uut.findUser(users, 'barney@'))  # email case insensitive
+
+        # find Betty
+        self.assertEqual(betty, uut.findUser(users, 'Betty R'))
+        self.assertEqual(betty, uut.findUser(users, 'betty.rubble@'))  # email case insensitive
